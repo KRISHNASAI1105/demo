2c46bdc325484be07a3716b5a37785b3afb4771d
KRISHNASAI1105 <75937326+KRISHNASAI1105@users.noreply.github.com>
2021-06-21 08:59:09 +0530

    Add files via upload

---
+++ AI/Python/SVM at y=0.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def svm_cost_0(x):
+    return np.array([0 if _ <= -1 else 0.26*(_ + 1) for _ in x])
+
+plt.plot(x, -np.log10(1-(1 / (1+np.exp(np.negative(x))))))
+plt.plot(x, svm_cost_0(x))
+plt.legend(['logistic regression cost function', 'modified SVM cost function'])
+plt.show()
